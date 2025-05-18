41cd83b493d5b4ecad793110edd1c5c2e274bbbe
Aqua <notnotsky26@gmail.com>
2025-05-18 05:41:41 +0000

    i think i fixed it

--- src/app/articlePage/page.tsx
+++ src/app/articlePage/page.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { supabase } from "@/lib/supabaseClient";
 
 const ArticlePage = ({ article, id }) => {
   const articleUrl = `https://nannuru.com/articles/${id}`;
@@ -10,7 +11,7 @@ const ArticlePage = ({ article, id }) => {
           <meta property="og:title" content={article.Heading} />
           <meta property="og:description" content={article.subHeading} />
           <meta property="og:url" content={articleUrl} />
-          <meta property="og:image" content={"https://bit.ly/3zzCTUT"} />
+          <meta property="og:image" content="https://bit.ly/3zzCTUT" />
           <meta property="og:type" content="article" />
           <meta property="og:site_name" content="Nannuru" />
         </Head>
@@ -24,5 +25,27 @@ const ArticlePage = ({ article, id }) => {
     </>
   );
 };
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+
+  const { data: article, error } = await supabase
+    .from("Nannuru_articles_table")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      article,
+      id,
+    },
+  };
+}
 
 export default ArticlePage;
