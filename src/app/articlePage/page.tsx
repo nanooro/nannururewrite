1419a649990e0d9a2da7a9de76ed7c0e2da1637c
Aqua <notnotsky26@gmail.com>
2025-05-20 14:08:35 +0000

    fixed the server side prop err thing

--- src/app/articlePage/page.tsx
+++ src/app/articlePage/page.tsx
@@ -1,21 +1,33 @@
-import Head from "next/head";
 import { supabase } from "@/lib/supabaseClient";
+import Head from "next/head";
 
-const ArticlePage = ({ article, id }) => {
-  const articleUrl = `https://nannuru.com/articles/${id}`;
+export default async function ArticlePage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { data: article, error } = await supabase
+    .from("Nannuru_articles_table")
+    .select("*")
+    .eq("id", params.id)
+    .single();
+
+  if (error || !article) {
+    return <div>Article not found</div>;
+  }
+
+  const articleUrl = `https://nannuru.com/articles/${params.id}`;
 
   return (
     <>
-      {article && (
-        <Head>
-          <meta property="og:title" content={article.Heading} />
-          <meta property="og:description" content={article.subHeading} />
-          <meta property="og:url" content={articleUrl} />
-          <meta property="og:image" content="https://bit.ly/3zzCTUT" />
-          <meta property="og:type" content="article" />
-          <meta property="og:site_name" content="Nannuru" />
-        </Head>
-      )}
+      <Head>
+        <meta property="og:title" content={article.Heading} />
+        <meta property="og:description" content={article.subHeading} />
+        <meta property="og:url" content={articleUrl} />
+        <meta property="og:image" content="https://bit.ly/3zzCTUT" />
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="Nannuru" />
+      </Head>
 
       {/* Article content
       <h1>{article.Heading}</h1>
@@ -24,28 +36,4 @@ const ArticlePage = ({ article, id }) => {
       <p>{article.content}</p> */}
     </>
   );
-};
-
-export async function getServerSideProps(context) {
-  const { id } = context.params;
-
-  const { data: article, error } = await supabase
-    .from("Nannuru_articles_table")
-    .select("*")
-    .eq("id", id)
-    .single();
-
-  if (error) {
-    console.error(error);
-    return { notFound: true };
-  }
-
-  return {
-    props: {
-      article,
-      id,
-    },
-  };
 }
-
-export default ArticlePage;
