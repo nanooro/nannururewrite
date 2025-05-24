c93c23b64151559e0b88faa4b98de7472a26771c
Aqua <notnotsky26@gmail.com>
2025-05-24 04:30:47 +0000

    fixed next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // skip linting during build
-  },
-  typescript: {
-    ignoreBuildErrors: true, // skip type checking during build
-  },
-=======
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
->>>>>>> 40e41e4 (init)
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
@@ -21,13 +16,10 @@ const nextConfig = {
       },
     ],
   },
-<<<<<<< HEAD
-=======
   webpack: (config) => {
     config.resolve.alias["@ui"] = path.resolve(__dirname, "src/components/ui");
     return config;
   },
->>>>>>> 40e41e4 (init)
 };
 
 module.exports = nextConfig;
