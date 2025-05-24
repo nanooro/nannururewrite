48af93db158fa45f919dbb61de7f5a26b1b87fc7
Aqua <notnotsky26@gmail.com>
2025-05-24 05:28:09 +0000

    fixed layout config

--- src/components/socials.tsx
+++
@@ -1,8 +0,0 @@
-import SocialCard from "@ui/socialCard";
-export default function Socials({ linkUrl, imgUrl, name }) {
-  return (
-    <div className=" w-auto h-auto " id="fbCard">
-      <SocialCard linkUrl={linkUrl} imgUrl={imgUrl} name={name} />
-    </div>
-  );
-}
