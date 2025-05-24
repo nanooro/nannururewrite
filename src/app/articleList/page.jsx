2f0f3c207b6beaf40c1638fe3e7eb3794d39ec76
Aqua <notnotsky26@gmail.com>
2025-05-24 05:55:54 +0000

    fixed layout config

--- src/app/articleList/page.jsx
+++ src/app/articleList/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabaseClient";
-
+import Header from "@ui/header";
 import Link from "next/link";
 
 export default function ArticlesPage() {
