bc3825c32ebac8fb52daf4b7c12f07862807a0b2
Aqua <notnotsky26@gmail.com>
2025-05-24 05:15:02 +0000

    fixed iports

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,4 +1,4 @@
-export function Footer() {
+function Footer() {
   return <div className="mt-24"></div>;
 }
 export { Footer };
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,7 +2,7 @@ import DropdownMenu from "@ui/dropDownMenu";
 import { motion } from "framer-motion";
 import Hero from "@ui/hero";
 import Link from "next/link";
-export function Header() {
+function Header() {
   return (
     <>
       <div className="flex justify-start items-center m-1   w-auto h-[8vh] bg-white">
