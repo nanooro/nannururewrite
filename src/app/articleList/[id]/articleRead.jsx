4fef404aae350da33e4b0cb9ef11990781650fa0
Aqua <notnotsky26@gmail.com>
2025-05-27 15:49:56 +0000

    fixed server side client side thing in supabase to fix teh img not showing up when shared on fb

--- src/app/articleList/[id]/articleRead.jsx
+++ src/app/articleList/[id]/articleRead.jsx
@@ -105,30 +105,30 @@ export default function ArticleRead() {
   function Footer() {
     return <div className="mt-24"></div>;
   }
-  import ArticleRead from "./ArticleRead";
-  import { supabase } from "../../../../lib/supabaseClient";
+  // // import ArticleRead from "./ArticleRead";
+  // import { supabase } from "../../../../lib/supabaseClient";
   
-  export async function generateMetadata({ params }) {
-    const { id } = params;
-    const { data } = await supabase
-      .from("Nannuru_articles_table")
-      .select("*")
-      .eq("id", id)
-      .single();
+  // export async function generateMetadata({ params }) {
+  //   const { id } = params;
+  //   const { data } = await supabase
+  //     .from("Nannuru_articles_table")
+  //     .select("*")
+  //     .eq("id", id)
+  //     .single();
   
-    return {
-      title: data?.Heading,
-      description: data?.subHeading,
-      openGraph: {
-        title: data?.Heading,
-        description: data?.subHeading,
-        images: [data?.imgUrl],
-        url: `https://www.nannuru.com/articleList/${id}`,
-        type: "article",
-      },
-    };
-  }
+  //   return {
+  //     title: data?.Heading,
+  //     description: data?.subHeading,
+  //     openGraph: {
+  //       title: data?.Heading,
+  //       description: data?.subHeading,
+  //       images: [data?.imgUrl],
+  //       url: `https://www.nannuru.com/articleList/${id}`,
+  //       type: "article",
+  //     },
+  //   };
+  // }
   
-  export default function Page() {
-    return <ArticleRead />;
-  }
+  // export default function Page() {
+  //   return <ArticleRead />;
+  // }
